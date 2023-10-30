765860e6e6034183a5a1c7ff1b509ae2d95c88fe
doshik04 <moharevilya@gmail.com>
2023-10-31 00:00:06 +0500

    first commit

---
+++ app/src/main/java/com/crest/basedataapp/Data.java
@@ -0,0 +1,14 @@
+package com.crest.basedataapp;
+
+public class Data {
+    String name;
+    String surname;
+    int year;
+
+    public Data(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+}
---
+++ app/src/main/java/com/crest/basedataapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.crest.basedataapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DBHelper dbHelper;
+    TextView tvOut;
+    EditText ename, esname, eyear, eNew;
+    Button  btnDel, btnAdd, btnGet, btnDelOne, btnUpdate;
+    @SuppressLint("CutPasteId")
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new DBHelper(this);
+
+        tvOut = findViewById(R.id.tvOut);
+
+        ename = findViewById(R.id.editName);
+        esname = findViewById(R.id.editSurname);
+        eyear = findViewById(R.id.editYear);
+        eNew = findViewById(R.id.editNew);
+
+        btnDel = findViewById(R.id.buttonDel);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnGet = findViewById(R.id.buttonGet);
+        btnDelOne = findViewById(R.id.buttonDelOne);
+        btnUpdate = findViewById(R.id.buttonUpdate);
+
+        btnDel.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnGet.setOnClickListener(this);
+        btnDelOne.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonDel:
+                dbHelper.DeleteAll();
+                break;
+            case R.id.buttonAdd:
+                String name = ename.getText().toString();
+                String sname = esname.getText().toString();
+                int year = Integer.parseInt(eyear.getText().toString());
+
+                Data data = new Data(name, sname, year);
+                dbHelper.AddOne(data);
+                break;
+            case R.id.buttonGet:
+                LinkedList<Data> list = dbHelper.GetAll();
+
+                String text = "";
+                for (Data d:list) text = text +(d.name)+ "  " +(d.surname)+ " "+ (d.year)+"\n";
+                tvOut.setText(text.toString());
+                break;
+            case R.id.buttonDelOne:
+                dbHelper.DeleteOne(eNew.getText().toString());
+                break;
+            case R.id.buttonUpdate:
+                dbHelper.UpdateOne(eNew.getText().toString());
+                break;
+        }
+
+    }
+}
